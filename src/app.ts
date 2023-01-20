83f57571ae55e0b72fbffe295e80e16d4fee5de4
MikeeOo <michal.olszewskki@gmail.com>
2023-01-20 13:32:16 +0100

    input_form: added event, got submit value, set input reset

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,44 @@
 import "../scss/main.scss"
 
+interface ITodoApp {
+    elements: IElements;
+    addTaskToList: (e: SubmitEvent) => void;
+}
+
+interface IElements {
+    taskForm: HTMLFormElement;
+    taskInput: HTMLInputElement;
+    tasksList: HTMLElement;
+}
+
+class TodoApp {
+
+    elements: IElements = {
+        taskForm: <HTMLFormElement>document.getElementById(`task_form`),
+        taskInput: <HTMLInputElement>document.getElementById(`task_form__input`),
+        tasksList: <HTMLElement>document.getElementById(`tasks_list`),
+    };
+
+    constructor() {
+
+        this.setEvents();
+    };
+
+
+    addTaskToList = (e: SubmitEvent): void => {
+        e.preventDefault();
+
+        console.log(e)
+        console.log(this.elements.taskInput.value);
+
+        this.elements.taskForm.reset();
+    };
+
+    setEvents(): void {
+        this.elements.taskForm.addEventListener(`submit`, this.addTaskToList);
+    };
+}
+
+const app: ITodoApp = new TodoApp();
+
+console.log(app);
