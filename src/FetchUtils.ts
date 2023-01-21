14dc5f9590a954d0735193b75d973659506450b7
MikeeOo <michal.olszewskki@gmail.com>
2023-01-21 18:12:02 +0100

    added: json-server, get & post functionality using htmlUtils class

---
+++ src/FetchUtils.ts
@@ -0,0 +1,31 @@
+export interface ITask {
+    taskName: string;
+    id: number;
+}
+
+export default class FetchUtils {
+
+    public baseUrl: string;
+
+    constructor(url: string) {
+        this.baseUrl = url;
+    };
+
+    get = async (endpoint: string): Promise<Array<ITask>> => {
+        return (await fetch(`${this.baseUrl}/${endpoint}`)).json()
+    }
+
+    post = async (
+        endpoint: string,
+        body: ITask,
+        headers: HeadersInit | undefined = {"Content-Type": "application/json"}
+        ): Promise<ITask> => {
+
+        return (await fetch(`${this.baseUrl}/${endpoint}`, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(body)
+        })).json()
+    }
+}
+
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import "../scss/main.scss"
+import FetchUtils from "./FetchUtils";
+import {ITask} from "./FetchUtils";
 
 interface ITodoApp {
     elements: IElements;
-    addTaskToList: (e: SubmitEvent) => void;
+    addTaskToApi: (e: SubmitEvent) => void;
+    fetchUtils: IFetchUtils;
 }
 
 interface IElements {
@@ -11,34 +14,63 @@ interface IElements {
     tasksList: HTMLElement;
 }
 
+interface IFetchUtils {
+    baseUrl: string;
+    get: (endpoint: string) => Promise<any>;
+    post: (endpoint: string, body: any, headers?: HeadersInit | undefined) => Promise<any>;
+}
+
 class TodoApp {
+
+    public fetchUtils: IFetchUtils;
 
     elements: IElements = {
         taskForm: <HTMLFormElement>document.getElementById(`task_form`),
         taskInput: <HTMLInputElement>document.getElementById(`task_form__input`),
-        tasksList: <HTMLElement>document.getElementById(`tasks_list`),
+        tasksList: <HTMLUListElement>document.getElementById(`tasks_list`),
     };
 
-    constructor() {
+    constructor(url: string) {
+        this.fetchUtils = new FetchUtils(url);
 
+        this.getTasks().then((r: Array<ITask>) => r.map((task: ITask) => this.createTaskListItem(task)));
+        // this.getTasks().then(r => console.log(r));
         this.setEvents();
     };
 
+    getTasks = async (): Promise<Array<ITask>> => await this.fetchUtils.get(`tasks`);
 
-    addTaskToList = (e: SubmitEvent): void => {
+    addTaskToApi = async (e: SubmitEvent): Promise<any> => {
         e.preventDefault();
 
-        console.log(e)
-        console.log(this.elements.taskInput.value);
+        // console.log(e);
+        // console.log(this.elements.taskInput.value);
+
+        const task = await this.fetchUtils.post(`tasks`, {
+            taskName: this.elements.taskInput.value
+        });
+
+        console.log(task)
 
         this.elements.taskForm.reset();
+
+        this.createTaskListItem(task);
     };
 
+    createTaskListItem = (task: ITask): void => {
+
+        const taskDiv: HTMLLIElement = document.createElement(`li`)
+        taskDiv.innerText = task.taskName
+        taskDiv.className = `taskLi`
+        this.elements.tasksList.appendChild(taskDiv)
+    }
+
     setEvents(): void {
-        this.elements.taskForm.addEventListener(`submit`, this.addTaskToList);
+        this.elements.taskForm.addEventListener(`submit`, this.addTaskToApi);
     };
 }
 
-const app: ITodoApp = new TodoApp();
+// new TodoApp();
+const app: ITodoApp = new TodoApp(`http://localhost:8000`);
 
 console.log(app);
