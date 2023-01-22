97f6ef3482305e68ede0616fc34cc98f725de5ea
MikeeOo <michal.olszewskki@gmail.com>
2023-01-22 17:31:50 +0100

    created full newTask html structure w/custom checkbox

--- src/HtmlUtils.ts
+++ src/HtmlUtils.ts
@@ -1,17 +1,22 @@
 interface IOptions {
     className?: string;
+    type?: string;
+    innerText?: string;
+    innerHTML?: string;
 }
 
+type HTMLElements = HTMLLIElement | HTMLInputElement | HTMLButtonElement ;
 
 export default class HtmlUtils {
 
-    static createHtmlElement = (selector: string, options: IOptions = {}): HTMLElement => {
+    static createHtmlElement = (selector: string, options: IOptions): HTMLElement => {
 
-        const el: HTMLElement = document.createElement(selector);
+        const el: HTMLElements = <HTMLElements>document.createElement(selector);
 
+        // console.log(el)
         for (const option in options){
-            console.log(option)
-            console.log(options)
+            // console.log(option)
+            // console.log(options)
             el[<keyof IOptions>option] = options[<keyof IOptions>option] as string;
         }
 
--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,6 @@ interface IFetchUtils {
 }
 
 class TodoApp {
-
     public fetchUtils: IFetchUtils;
 
     elements: IElements = {
@@ -34,37 +33,47 @@ class TodoApp {
     constructor(url: string) {
         this.fetchUtils = new FetchUtils(url);
 
-        this.getTasks().then((r: Array<ITask>) => r.map((task: ITask) => this.createTaskListItem(task)));
+        this.getTasks().then((r: Array<ITask>) => r.map((task: ITask) => this.createTasksListItem(task)));
         // this.getTasks().then(r => console.log(r));
         this.setEvents();
     };
 
     getTasks = async (): Promise<Array<ITask>> => await this.fetchUtils.get(`tasks`);
 
-    addTaskToApi = async (e: SubmitEvent): Promise<any> => {
+    addTaskToApi = async (e: SubmitEvent): Promise<void> => {
         e.preventDefault();
-
-        // console.log(e);
-        // console.log(this.elements.taskInput.value);
 
         const task = await this.fetchUtils.post(`tasks`, {
             taskName: this.elements.taskInput.value
         });
 
-        console.log(task)
+        console.log(task);
 
         this.elements.taskForm.reset();
 
-        this.createTaskListItem(task);
+        this.createTasksListItem(task);
     };
 
-    createTaskListItem = (task: ITask): void => {
-        const listItem: HTMLElement = HtmlUtils.createHtmlElement(`li`, {className: `tasks_list__task_item`})
+    createTasksListItem = (task: ITask): void => {
 
-        listItem.innerText = task.taskName;
+        const listItem: HTMLElement = HtmlUtils.createHtmlElement(`li`, {className: `tasks_list__task_item`});
+
+            const checkBoxLabel = HtmlUtils.createHtmlElement(`label`, {className: `checkbox`});
+
+        listItem.appendChild(HtmlUtils.createHtmlElement(`div`, {className: `task_checker`}))
+                .appendChild(checkBoxLabel);
+
+                checkBoxLabel.appendChild(HtmlUtils.createHtmlElement(`input`, {type: `checkbox`}));
+
+                checkBoxLabel.appendChild(HtmlUtils.createHtmlElement(`span`, {className: `checkmark`}));
+
+        listItem.appendChild(HtmlUtils.createHtmlElement(`span`, {innerText: task.taskName}));
+
+        listItem.appendChild(HtmlUtils.createHtmlElement(`button`, {}))
+                .appendChild(HtmlUtils.createHtmlElement(`span`, {className: `fa-solid fa-plus`}));
 
         this.elements.tasksList.appendChild(listItem);
-    }
+    };
 
     setEvents(): void {
         this.elements.taskForm.addEventListener(`submit`, this.addTaskToApi);
@@ -72,6 +81,6 @@ class TodoApp {
 }
 
 // new TodoApp();
-const app: ITodoApp = new TodoApp(`http://localhost:9999`);
+const app: TodoApp = new TodoApp(`http://localhost:9999`);
 
 console.log(app);
